37571cd9bd2709cec44dd5f75c84d8e23fe74e1f
Robert Wakefield <r.wakefield1987@gmail.com>
2023-08-21 22:51:16 +0100

    intergrated home and login to flask with extend

--- Trident/trident.py
+++ Trident/trident.py
@@ -3,7 +3,7 @@ from mimetypes import MimeTypes
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route('/')
+@app.route('/home')
 
 def home():
     return render_template('home.html')
