4cf7d6bc936816b2810223501feeb2447814d69c
Robert Wakefield <r.wakefield1987@gmail.com>
2023-08-22 23:04:00 +0100

    moved all pages to flask changed map api and css

--- Trident/trident.py
+++ Trident/trident.py
@@ -1,9 +1,24 @@
 from flask import Flask, Response, render_template, request, redirect
 from mimetypes import MimeTypes
+import os
+import random
+
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route('/home')
+
+@app.route('/index')
+def index():
+    background_images_folder = os.path.join(app.static_folder, 'bgimg')
+    image_files = [f for f in os.listdir(background_images_folder) if f.endswith(('jpg', 'png', 'jpeg'))]
+    if not image_files:
+        return "No background images found."
+    random_image = random.choice(image_files)
+    background_image_url = f"static/bgimg/{random_image}"
+    print(background_image_url)  # Check if the URL is correct
+    return render_template('index.html', background_image_url=background_image_url)
+
+@app.route('/')
 
 def home():
     return render_template('home.html')
