07fdc32decc07bd227389217d15e3293ed96e9b2
Robert Wakefield <r.wakefield1987@gmail.com>
2023-08-24 15:57:07 +0100

    registration page setup including db interg/css

--- forms.py
+++ forms.py
@@ -8,50 +8,10 @@ from passlib.hash import sha256_crypt
 import re
 from flask import render_template
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
-@app.route('/index')
-def index():
-    background_images_folder = os.path.join(app.static_folder, 'bgimg')
-    image_files = [f for f in os.listdir(background_images_folder) if f.endswith(('jpg', 'png', 'jpeg'))]
-    if not image_files:
-        return "No background images found."
-    random_image = random.choice(image_files)
-    background_image_url = f"static/bgimg/{random_image}"
-    print(background_image_url)  # Check if the URL is correct
-    return render_template('index.html', background_image_url=background_image_url)
 
-@app.route('/')
-
-def home():
-    return render_template('home.html')
-
-@app.route('/maps')
-
-def maps():
-    return render_template('maps.html')
-
-@app.route('/sports')
-
-def sports():
-    return render_template('sports.html')
-
-@app.route('/login')
-
-def login():
-    return render_template('login.html')
-
-
-@app.route('/signup')
-
-def signup():
-    return render_template('signup.html')
-
-@app.route('/contact')
-
-def contact():
-    return render_template('contact.html')
 
 
 def Connection():
@@ -96,5 +56,4 @@ def registration():
 
     return render_template('registration.html', msg=msg)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
